2c62399aa0864ef28aee08b6a26c3fe2a2dc5bd6
sergey <sergey.kobelew@yandex.ru>
2026-02-14 20:50:53 +0300

    add root route hello world

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -9,6 +9,10 @@ plugins {
 group = "hexlet.code"
 version = "1.0-SNAPSHOT"
 
+application {
+    mainClass.set("hexlet.code.App")
+}
+
 repositories {
     mavenCentral()
 }
@@ -24,7 +28,12 @@ tasks.test {
 }
 
 dependencies {
-    implementation("info.picocli:picocli:4.7.7")
+    implementation("io.javalin:javalin:6.7.0")
+    implementation("org.slf4j:slf4j-simple:2.0.17")
+    compileOnly("org.projectlombok:lombok:1.18.42")
+    annotationProcessor("org.projectlombok:lombok:1.18.42")
+    testCompileOnly("org.projectlombok:lombok:1.18.42")
+    testAnnotationProcessor("org.projectlombok:lombok:1.18.42")
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
---
+++ app/src/main/java/hexlet/code/App.java
@@ -0,0 +1,22 @@
+package hexlet.code;
+
+import io.javalin.Javalin;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public final class App {
+    private App() {
+    }
+
+    public static Javalin getApp() {
+        var app = Javalin.create();
+        app.get("/", ctx -> ctx.result("Hello World"));
+        return app;
+    }
+
+    public static void main(String[] args) {
+        var app = getApp();
+
+        app.start();
+    }
+}
