30624503ca23f2db704597be8f1398c33d729dde
sergey <sergey.kobelew@yandex.ru>
2026-02-14 15:28:56 +0300

    Intital commit

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 ### Hexlet tests and linter status:
 [![Actions Status](https://github.com/SergeyKkk1/java-project-72/actions/workflows/hexlet-check.yml/badge.svg)](https://github.com/SergeyKkk1/java-project-72/actions)
+[![Quality Gate Status](https://sonarcloud.io/api/project_badges/measure?project=SergeyKkk1_java-project-72&metric=alert_status)](https://sonarcloud.io/summary/new_code?id=SergeyKkk1_java-project-72)
+[![Coverage](https://sonarcloud.io/api/project_badges/measure?project=SergeyKkk1_java-project-72&metric=coverage)](https://sonarcloud.io/summary/new_code?id=SergeyKkk1_java-project-72)
+## Web pages analyzer
+A free project which helps analyze web pages and see if your site 
+has good SEO or not.
+## How to start this project.
+```
+git@github.com:SergeyKkk1/java-project-72.git
+make build
+```
---
+++ app/build.gradle.kts
@@ -0,0 +1,42 @@
+plugins {
+    application
+    checkstyle
+    id("org.sonarqube") version "7.0.1.6134"
+    id ("com.github.ben-manes.versions") version "0.53.0"
+    jacoco
+}
+
+group = "hexlet.code"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+tasks.jacocoTestReport {
+    reports {
+        xml.required.set(true)
+    }
+}
+
+tasks.test {
+    finalizedBy(tasks.jacocoTestReport)
+}
+
+dependencies {
+    implementation("info.picocli:picocli:4.7.7")
+    testImplementation(platform("org.junit:junit-bom:5.10.0"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+
+sonar {
+    properties {
+        property("sonar.projectKey", "SergeyKkk1_java-project-72")
+        property("sonar.organization", "sergeykkk1")
+    }
+}
